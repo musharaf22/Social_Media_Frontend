7c437fe920880276157b398a0d83393fca444e56
musharaf-fleapo <musharaf@fleapo.com>
2022-11-02 22:06:51 +0530

    making Ui better

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -6,24 +6,27 @@ const Post = () => {
   const [like, setlike] = useState<boolean>(false);
   return (
     <div className="w-full rounded-lg bg-white h-auto flex flex-col shadow-xl mb-6 ">
-      <div className="image">
+      <div className="image p-4">
         <img
           src="/images/postpic2.jpg"
           alt="N/A"
           className="w-full h-auto rounded-lg"
         />
       </div>
-      <div className="icons flex w-[50%] justify-evenly my-5 ">
-        {!like && <BsHeart size={30} onClick={() => setlike(true)} />}
+      <div className="icons flex w-[50%] justify-items-start my-5 ">
+        {!like && (
+          <BsHeart className="ml-6" size={30} onClick={() => setlike(true)} />
+        )}
         {like && (
           <BsFillHeartFill
+            className="ml-6"
             size={30}
             color="#FF812C"
             onClick={() => setlike(false)}
           />
         )}
-        <FaRegCommentDots size={30} />
-        <FiShare2 size={30} />
+        <FaRegCommentDots className="ml-6" size={30} />
+        <FiShare2 className="ml-6" size={30} />
       </div>
       <div className="text ml-6 mb-8">
         <p className="text-xl text-gray-400">2000 Likes</p>
